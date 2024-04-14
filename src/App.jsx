21091cfe3b862bdfbdda04e38572e9ae2c3af765
LeKhanhBaoKha <baokha1024.com@gmail.com>
2024-04-14 14:02:24 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import './App.css'
+import { faHouse } from '@fortawesome/free-solid-svg-icons/faHouse'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGamepad, faStore, faTasks, faVideo } from '@fortawesome/free-solid-svg-icons'
+
+function App() {
+  const [active, setActive] = useState("Home")
+
+  const tabs = [
+    {
+      name: "Home", icon: faHouse
+    },
+    {
+      name: "Video", icon: faVideo
+    },
+    {
+      name: "Store", icon: faStore
+    },
+    {
+      name: "Game", icon: faGamepad
+    },
+    {
+      name: "Task", icon: faTasks
+    },
+  ]
+  return (
+    <>
+      <div className='Header flex justify-between h-[56px] gap-[5px] bg-[#242526] w-[100vw]'>
+        <div className='Search flex'>
+          <div className='flex flex-shrink-0 items-center w-[40px] ml-2'>
+            <img className='w-[100%]' src='https://upload.wikimedia.org/wikipedia/commons/b/b9/2023_Facebook_icon.svg'></img>
+            <search className='ml-2'>
+              <form>
+                <input className='h-[30px] rounded-2xl bg-[#3a3b3c] pl-4 text-indent-4' placeholder='Tìm kiếm'></input>
+              </form>
+            </search>
+          </div>
+        </div>
+
+        <div className="flex justify-center ">
+          {tabs.map(({ name, icon }) => (
+            <div key={name} className={`hover:cursor-pointer icon-container ${active == name ? "active" : ""}`} onClick={() => setActive(name)}>
+              <FontAwesomeIcon icon={icon} className={`w-[20px] h-[20px] p-[18px] ${active == name ? "text-[#0866ff]" : "text-gray-400"}`} />
+            </div>
+          ))}
+        </div>
+
+        <div className='userbutton'>
+
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
